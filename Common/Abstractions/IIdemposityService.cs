a19bce57e118cc9597d2cf5efd1a7a185f7ccc7c
la_Peregrin <KulykVA@krok.edu.ua>
2022-05-08 18:20:16 +0300

    Idempotisity abstractions

---
+++ Common/Abstractions/IIdemposityService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Common.Abstractions
+{
+    public interface IIdemposityService<TId, TTaskResult>
+    {
+        Task<TTaskResult> TryAddProcess(TId id, Task<TTaskResult> task);
+        Task<TTaskResult> TryAddPriorityProcess(TId id, Task<TTaskResult> task);
+    }
+}
